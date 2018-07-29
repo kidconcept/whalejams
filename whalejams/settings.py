7b566fc920eaa4d5c54fc12df181a49ae6eaa520
kidconcept <me@kidconcept.com>
2018-07-29 23:42:20 +0200

    INSTALLED_APPS guesswork fixedd

--- whalejams/settings.py
+++ whalejams/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-storages',
+    'storages',
     'boto3',
     'progressbarupload',
     'ohwhale.apps.OhwhaleConfig',
